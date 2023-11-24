4bffbc8ba50c0daeef1201f41ffaa3d7ba4597ad
maroo19 <mcheema.bese21seecs@seecs.edu.pk>
2023-11-24 16:25:45 +0500

    test cases in graphpoetest.java

--- ps2/test/poet/GraphPoetTest.java
+++ ps2/test/poet/GraphPoetTest.java
@@ -20,6 +20,29 @@ public class GraphPoetTest {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
-    // TODO tests
+    @Test
+    public void testRepeatedWordsGraphPoet() {
+        List<String> lines = Arrays.asList(
+                "I love programming",
+                "Programming is fun",
+                "Fun with Java",
+                "Java is versatile",
+                "Versatile programming is fun too"
+        );
+
+        GraphPoet poet = new GraphPoet(lines);
+
+        String input = "I love programming";
+        String expected = "I love versatile programming";
+        assertEquals(expected, poet.poem(input));
+    }
+
+    @Test
+    public void testEmptyGraphPoet() {
+        GraphPoet poet = new GraphPoet(Arrays.asList());
+
+        String input = "I love programming";
+        assertEquals(input, poet.poem(input));
+    }
     
 }
